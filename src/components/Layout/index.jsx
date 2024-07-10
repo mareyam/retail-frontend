6b203f3e71daf4fc8c97e942c116d211de435a6c
maryam <maryaam22092@gmail.com>
2024-07-10 09:29:51 -0700

    iis configrations

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <Flex>
+        <Box h='100dvh' pos='fixed' flex='0.3'></Box>
+        <Box h='100dvh' pos='fixed' flex='0.7'></Box>
+    </Flex>
+    
+  )
+}
+
+export default Layout
